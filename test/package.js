fe13d62be304917610c5c449a6c363d6f168a293
E.Azer Koçulu <azer@kodfabrik.com>
2012-06-25 17:58:44 -0700

    better lookin package.js

--- test/package.js
+++ test/package.js
@@ -1,4 +1,5 @@
 var assert            = require('assert'),
+    compose           = require('functools').compose,
     one               = require('../lib/'),
 
     utils             = require('./common'),
